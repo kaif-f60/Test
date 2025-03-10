5957cfaa944efb802ef99d537a777f4e7fbbddd8
kaif-f60 <kaif@pentagoat.com>
2025-03-10 13:08:25 +0530

    this is first commits

---
+++ main.js
@@ -0,0 +1,220 @@
+// const mbmenu = doocument.getElementById()
+
+// const bike={
+//     type: "Bike",
+//     company: "Hero",
+//     number:"mh-56-ad-6586",
+//     color: " Red ",
+//     year:"2005",
+//     model:"BH1",
+//     engine:"bs6",
+
+// }
+
+// function de(){
+//     console.log("i Purchased A" + bike.company + "which is" + 
+//     bike.color + "and bike number is" + bike.number )
+// }
+// const date = new Date();
+// console.log(date);
+//  let a=[ 1,2,3,4,5,6,7 ];
+// //  console.log(a[5]); // it will show error because the array
+// function printarray() {
+//     document.getElementsByClassName("test").innerHTML = a[0];
+// }
+// console.log(printarray)
+
+// let Arrray=["a","b","c","d","e","f","g","h","i","j"];
+ 
+// let Array2=["1","2","3","4","5","6","7","8",];
+
+
+// let ThirdArray=Arrray.concat(Array2)
+// function Newf(){
+
+// document.getElementById("ko").innerHTML=ThirdArray;
+// console.log(ThirdArray);
+// }
+
+
+
+// console.log(Array2, Array2)
+
+// let value1 = 52;
+// let value2 = 758;
+// let value3 = 588;
+// let value4 = 693;
+
+// let addition = () => {
+//     let total = `This is Value one ${value1}, This is Value ${value2}, This is Value ${value3} , This is Value ${value2}`;
+//     console.log(total)   
+// }
+// addition()
+
+
+// console.log("thirdArray")
+
+// const Suppliers1 = {
+//     Name: "Shrikant Steel Centre",
+//     Number:"7845652589",
+//     Gst:"12345xyzzAbcd",
+//     Add:"Xyz 1st Floor",
+//     Products : {
+//         Flanges: 5,
+//         Roundbars: 10,
+//         Pipe:1,
+//         Sheet:2,
+//         Coil:6,
+//     }
+// }
+
+// Suppliers1.neprop= hello() {
+//     return(this.Name + "We are learning" + this.Products[Flanges]).toUpperCase()};
+// document.getElementById("lo")
+// const a1 ={
+// name: "abid",
+// age : "43"
+// };
+// const a2 ={
+//     name: "asif",
+//     age : "56"
+// };
+
+    
+// const a3 ={
+//     name: "arif",
+//     age : "45",
+//     test: function(){
+//         return a1.name + a3.name;
+        
+//     }
+//     };
+// let a4 = a3.test.call(a1) ;
+// console.log(a4)
+
+// const new1= ['potato', 78, 'apple', 67, 'watermelon', 450, 'banana', 897];
+// const new2= ['red', 561, 'yellow', 56, 'green', 450, 'black', 897];
+// function foo(){
+//     document.getElementById('four').innerHTML=new1.indexOf[450];
+//     let hhji = new1.indexOf[450];
+//     console.log(hhji)  
+// }   
+
+
+// // setInterval(foo,5000)
+// let myPromise = new Promise(function(myResolve, myReject) {
+//     // "Producing Code" (May take some time)
+    
+//       myResolve(
+
+//         console.log('Promise is resolved' )
+//       ); // when successful
+//       myReject(  console.log('Promise is reject' )
+//     );  // when error
+//     });
+    
+//     // "Consuming Code" (Must wait for a fulfilled Promise)
+//     myPromise.then(
+//       function(value) { /* code if successful */ },
+//       function(error) { /* code if some error */ }
+//     );
+
+
+// function ab(){
+// let a = 8;
+// let b = 9;
+//   console.log(a+b);
+ 
+// };
+//  document.getElementById('ko').innerHTML=ab();
+
+// const arr1 = []
+// arr1[1] = 'hllo';
+// arr1[2] = 'ello';
+// arr1[3] = 'hllo';
+// arr1[4] = 'hell';
+// arr1[5] = 'elo';
+// arr1[6] = 'helo';
+// arr1[7] = 'heo';
+// arr1[8] = 'hello';
+// arr1[9] = 'ho';
+
+// console.log(arr1) 
+
+// const hh = arr1;
+
+// const btn = document.getElementById('new');
+
+// // Add a click event listener
+// btn.addEventListener('click', function ggg() {
+//     // Change styles of the element with ID "new"
+//     const newElement = document.getElementById("new");
+//     newElement.style.color = "red";
+//     newElement.style.backgroundColor = "green";
+//     newElement.style.borderRadius = "10px";
+//     newElement.style.padding = "50px";
+//     newElement.style.transition = "all 0.3s ease";  // Adding smooth transition
+// });
+
+
+  
+
+
+
+// function chocolate (a,b) {
+//    return a+b ;
+// };
+
+
+
+// function vanilla (x,y,z,chocolate){
+//     return x*y-z ;
+//     chocolate();
+// };
+
+
+// let rraay = vanilla(9,9, 8, chocolate)
+// console.log(rraay);
+
+
+
+
+// const para1 = "This is first paragraph and"
+
+// if (para1.includes('first')){
+//     console.log("The paragraph contains the word 'first'");
+// }
+
+const para1 = "This is first paragraph and";
+
+if (para1.includes('not')) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+
+function testinng(){
+    const para1 = "This is first paragraph and";
+
+    if (para1.includes('not')) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+    
+}
+
+if (testinng().includes(para1)){
+    console.log('ha mere paas hai');
+} else {
+    console.log('nhi main nhi rakhat')
+}
+
+
+
+
+   
+
+
+           
